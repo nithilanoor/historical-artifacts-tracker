b036b630ea0f7fa2dee956a773af2a281da651b8
nithilanoor <nithilanoorchowdhury@gmail.com>
2024-12-24 23:13:22 +0600

    Featured Artifacts

--- src/components/ArtifactCard.jsx
+++ src/components/ArtifactCard.jsx
@@ -1,8 +1,8 @@
-
+import { IoMdHeart } from "react-icons/io";
 
 const ArtifactCard = ({ art }) => {
 
-    const { name, image, createdAt, discoveredBy, presentLocation } = art;
+    const { name, image, historicalContext, likeCount } = art;
 
     return (
         <div className="card card-compact bg-base-100 shadow-xl">
@@ -14,9 +14,8 @@ const ArtifactCard = ({ art }) => {
             </figure>
             <div className="card-body">
                 <h2 className="text-2xl font-serif font-semibold text-[#E2B13C]">{name}</h2>
-                <p>Created At: {createdAt}</p>
-                <p>Discovered By: {discoveredBy}</p>
-                <p>Present Location: {presentLocation}</p>
+                <p><span className="font-bold">Historical Context:</span> {historicalContext}</p>
+                <p className="flex items-center gap-1 mt-1"><IoMdHeart className="text-[#E2B13C]" />Likes: {likeCount}</p>
                 <div className="card-actions justify-start">
                     <button className="btn bg-[#E2B13C] text-white hover:bg-white hover:text-[#E2B13C]">View Details</button>
                 </div>
--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
+import useAxios from "../hooks/useAxios";
+import ArtifactCard from "./ArtifactCard";
+import { IoIosArrowForward } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 
 const Featured = () => {
+
+    const [artifacts, setArtifacts] = useState([]);
+    const axiosSecure = useAxios();
+
+
+    useEffect(() => {
+        axiosSecure.get('/featured/artifacts')
+            .then(res => {
+                console.log(res.data);
+                setArtifacts(res.data)
+            })
+    }, [])
+
     return (
         <div className="my-12">
-            Featured Artifacts
+            <h2 className="text-3xl font-semibold text-center">Featured <span className="text-[#E2B13C]">ARTIFACTS</span></h2>
+            <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-4 my-8">
+                {
+                    artifacts.map(art => <ArtifactCard key={art._id} art={art}></ArtifactCard>)
+                }
+            </div>
+            <div className="flex justify-center my-10">
+                <Link to="allArtifacts"><button className="border border-[#E2B13C] lg:w-60 px-16 py-2 font-semibold text-[#E2B13C] flex items-center justify-center gap-2 hover:bg-[#E2B13C] hover:text-white">See All <IoIosArrowForward /></button></Link>
+            </div>
         </div>
     );
 };
