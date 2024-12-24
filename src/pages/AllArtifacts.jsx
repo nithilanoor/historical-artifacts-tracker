5e1d70d04febfe10a2e3711247682b781393a960
nithilanoor <nithilanoorchowdhury@gmail.com>
2024-12-24 21:46:48 +0600

    JSON Data

--- src/pages/AllArtifacts.jsx
+++ src/pages/AllArtifacts.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import useAxios from "../hooks/useAxios";
 
 const AllArtifacts = () => {
+
+    const axiosSecure = useAxios();
+    const [artifacts, setArtifacts] = useState([]);
+
+
+    useEffect(() => {
+        axiosSecure.get('/artifacts')
+        .then(res => {
+            console.log(res.data);
+        })
+    }, [])
+
     return (
         <div className="w-11/12 mx-auto">
             <nav>
