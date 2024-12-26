1415cebe4ccf0f96b3344f9ef32eb346f02e932a
nithilanoor <nithilanoorchowdhury@gmail.com>
2024-12-26 18:30:41 +0600

    Delete Functionality

--- src/pages/MyArtifacts.jsx
+++ src/pages/MyArtifacts.jsx
@@ -4,7 +4,8 @@ import Navbar from "../components/Navbar";
 import useAuth from "../hooks/useAuth";
 import useAxios from "../hooks/useAxios";
 import { MdDeleteForever, MdOutlineModeEditOutline } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 
 const MyArtifacts = () => {
@@ -12,6 +13,7 @@ const MyArtifacts = () => {
     const [artifacts, setArtifacts] = useState([]);
     const { user } = useAuth();
     const axiosSecure = useAxios();
+    const navigate = useNavigate();
 
     useEffect(() => {
         axiosSecure.get(`/artifacts?email=${user.email}`)
@@ -19,7 +21,40 @@ const MyArtifacts = () => {
                 console.log(res.data)
                 setArtifacts(res.data)
             })
-    }, [user.email])
+    }, [user.email]);
+
+
+    // Delete Functionality
+    const handleDelete = _id => {
+        // console.log(_id);
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#E2B13C",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/artifacts/${_id}`)
+                    .then(res => {
+                        console.log(res.data);
+                        if (res.data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your posted artifact has been deleted.",
+                                icon: "success"
+                            });
+                            navigate('/allArtifacts')
+                        }
+                    })
+
+            }
+        });
+    }
+
 
     return (
         <div className="w-11/12 mx-auto">
@@ -45,14 +80,14 @@ const MyArtifacts = () => {
                                 </thead>
                                 <tbody>
                                     {artifacts.map((art, index) => (
-                                        <tr key={art.id || index}>
+                                        <tr key={art._id || index}>
                                             <th className="text-[#e2b13c]">{index + 1}</th>
                                             <td>{art.addedBy}</td>
                                             <td>{art.name}</td>
                                             <td>{art.type}</td>
                                             <td>{art.presentLocation}</td>
                                             <td>
-                                                <button className="mx-1 btn text-white btn-error btn-xs"><MdDeleteForever /></button>
+                                                <button onClick={() => handleDelete(art._id)} className="mx-1 btn text-white btn-error btn-xs"><MdDeleteForever /></button>
                                                 <Link to={`/update/${art._id}`}><button className=" btn text-white bg-[#e2b13c] btn-xs"><MdOutlineModeEditOutline /></button></Link>
                                             </td>
                                         </tr>
